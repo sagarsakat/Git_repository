1686c67e9b95aa439ffebd4e60884cfe1de9ac88
sagarsakat <sagarsakat511@gmail.com>
2023-04-21 10:54:25 +0530

    commit

--- PatternABCD.java
+++ PatternABCD.java
@@ -1,6 +1,6 @@
 public class PatternABCD {
 
-	public static void main(StringExample[] args) {
+	public static void main(String []args) {
 		for (int i=5;i>=1;i--)
 		{   char a='A';
 			for(int j=1;j<=i;j++)
